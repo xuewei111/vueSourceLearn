a45177e9815be98efc091450649a7aa3e9de2eb8
xuewei111 <m13358109269_1@163.com>
2021-08-19 13:12:18 +0800

    20210819

---
+++ vue_test/README.md
@@ -0,0 +1,9 @@
+<!--
+ * @Descripttion: 
+ * @Author: xuewei
+ * @version: 
+ * @Date: 2021-08-18 18:09:47
+ * @LastEditors: xuewei
+ * @LastEditTime: 2021-08-18 18:17:13
+-->
+#笔记
---
+++ vue_test/vue.config.js
@@ -0,0 +1,16 @@
+/*
+ * @Descripttion: 
+ * @Author: xuewei
+ * @version: 
+ * @Date: 2021-08-19 12:57:39
+ * @LastEditors: xuewei
+ * @LastEditTime: 2021-08-19 13:00:41
+ */
+module.exports = {
+    pages: {
+      index: {
+        // page 的入口
+        entry: 'src/main.js',
+      },
+    }
+  }

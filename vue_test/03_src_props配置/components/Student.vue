b1d7d0edbe6735c1289a8183c3d944fd17df6f4d
xuewei111 <m13358109269_1@163.com>
2021-08-22 13:27:34 +0800

    20210822

---
+++ vue_test/03_src_props配置/components/Student.vue
@@ -0,0 +1,60 @@
+<!--
+ * @Descripttion: 
+ * @Author: xuewei
+ * @version: 
+ * @Date: 2021-08-21 22:37:11
+ * @LastEditors: xuewei
+ * @LastEditTime: 2021-08-22 13:13:11
+-->
+<!--  -->
+<template>
+  <div>
+      <h1>{{msg}}</h1>
+      <h2>学生姓名: {{name}}</h2>
+      <h2>学生性别: {{sex}}</h2>
+      <h2>学生年龄: {{myAge+1}}</h2>
+
+      <button @click="update">尝试修改收到的年龄</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'Student',
+  data () {
+    return {
+        msg:'一个叫张三的人',
+        myAge:this.age
+    };
+  },
+  // 简单声明接收
+  // props:['name','age','sex'] 
+  // 接收的同时对数据进行类型限制
+  // props:{
+  //   name:String,
+  //   age:Number,
+  //   sex:String
+  // }
+  // 接收的同时对数据:进行类型限制+默认值的指定+必要值的限制
+  props:{
+    name:{
+      type:String, //name的类型是字符串
+      required:true // name是必须的
+    },
+    age:{
+      type:Number,
+      default:99 // 默认值
+    },
+    sex:{
+      type:String,
+      required:true
+    }
+  },
+  methods:{
+    update(){
+      this.myAge++
+    }
+  }
+}
+
+</script>
--- vue_test/README.md
+++ vue_test/README.md
@@ -4,7 +4,7 @@
  * @version: 
  * @Date: 2021-08-18 18:09:47
  * @LastEditors: xuewei
- * @LastEditTime: 2021-08-21 23:45:49
+ * @LastEditTime: 2021-08-22 13:19:56
 -->
 #笔记
 
@@ -14,3 +14,29 @@
     3.使用方式:
         打标识: <h1 ref="xxx">...</h1> 或 <School ref="xxx"></School>
         获取: this.$refs.xxx
+
+
+## 配置项props
+    功能: 让组件接收外部传过来的数据
+        1.传递数据:
+            <demo name="xxxx"/>
+        2.接收数据:
+            第一种方式(只接收):
+                props:['name']
+
+            第二种方法(限制类型)
+                props:{
+                    name:Number
+                }
+            第三种方法(限制类型、限制必要性、指定默认值)
+                props:{
+                    name:{
+                        type:String, //类型
+                        required:true, //必要性
+                        default:'老王' //默认值
+                    }
+                }
+
+    备注: props是只读的,Vue底层会监测你对props的修改,如果进行了修改,就会发出警告,
+            若业务需求确实需要修改,那么请复制props的内容到data中一份,然后去修改data中
+            的数据

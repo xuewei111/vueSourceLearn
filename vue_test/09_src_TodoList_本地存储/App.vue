41e2c29651a677175b2d0ad40daf20d83bfe2bf2
xuewei111 <m13358109269_1@163.com>
2021-09-07 21:49:36 +0800

    20210907

---
+++ vue_test/09_src_TodoList_本地存储/App.vue
@@ -0,0 +1,136 @@
+<!--
+ * @Descripttion: 
+ * @Author: xuewei
+ * @version: 
+ * @Date: 2021-08-20 23:23:34
+ * @LastEditors: xuewei
+ * @LastEditTime: 2021-09-01 07:07:47
+-->
+<!--  -->
+<template>
+  <div id="root"> 
+    <div class="todo-container">
+      <div class="todo-wrap">
+        <MyHeader :addTodo = "addTodo"/>
+        <MyList :todos = "todos" :checkTodo="checkTodo" :deleteTodo="deleteTodo"/>
+        <MyFooter :todos="todos" :checkAllTodo="checkAllTodo" :clearAllTodo="clearAllTodo"/>
+      </div>
+    </div>
+    
+  </div>
+</template>
+
+<script>
+// 引入School组件
+import MyHeader from './components/MyHeader'
+import MyList from './components/MyList'
+import MyFooter from './components/MyFooter'
+
+export default {
+  name:'App',
+  components:{
+    MyHeader,
+    MyList,
+    MyFooter
+  },
+  data () {
+      return {
+        // 由于todos是MyHeader组件和MyFooter组件 都在使用,所以放在App中(状态提升)
+        todos:JSON.parse(localStorage.getItem('todos')) || []
+      };
+  },
+  methods: {
+    // 添加一个todo
+    addTodo(x){
+      console.log('我是App组件,我收到了数据:',x)
+      this.todos.unshift(x)
+    },
+    // 勾选 or 取消勾选 一个todo
+    checkTodo(id){
+      // this.todos.filter(x=>x.id===id)[0].done = !
+      this.todos.forEach(x=>{
+        if(x.id===id){
+          x.done = !x.done
+        }
+      })
+    },
+    // 删除一个todo
+    deleteTodo(id){
+      this.todos = this.todos.filter(todo=> todo.id !== id)
+    },
+    //全选 or 取消全选
+    checkAllTodo(done){
+      this.todos.forEach(todo => {
+        todo.done = done
+      })
+    },
+    // 清除所有已经完成的todo
+    clearAllTodo(){
+      this.todos = this.todos.filter(todo=>{
+        return !todo.done
+      })
+    }
+  },
+  watch:{
+    // todos(value){
+    //   localStorage.setItem('todos',JSON.stringify(value))
+    // }
+    todos:{
+      deep:true,
+      handler(value){
+        localStorage.setItem('todos',JSON.stringify(value))
+      }
+    }
+  }
+}
+
+</script>
+<style>
+  /** base */
+  body {
+    background: #fff;
+  }
+  
+  .btn {
+    display: inline-block;
+    padding: 4px 12px;
+    margin-bottom: 0;
+    font-size: 14px;
+    line-height: 20px;
+    text-align: center;
+    vertical-align: middle;
+    cursor: pointer;
+    box-shadow: inset 0 1px 0 rgba(255,255,255,0.2), 0 1px 2px rgba(0,0,0,0.05);
+    border-radius: 4px;
+  }
+
+  .btn-danger {
+    color: #fff;
+    background-color: #da4f49;
+    border: 1px solid #bd352f
+  }
+
+  .btn-danger:hover {
+    color: #fff;
+    background-color: #bd352f;
+  }
+
+  .btn:focus {
+    outline: none;
+  }
+  
+  .todo-container {
+    width: 600px;
+    margin: 0 auto;
+  }
+
+  .todo-container .todo-wrap {
+    padding: 10px;
+    border: 1px solid #ddd;
+    border-radius: 5px;
+  }
+
+
+
+
+</style>
---
+++ vue_test/src/components/School.vue
@@ -0,0 +1,37 @@
+<!--
+ * @Descripttion: 
+ * @Author: xuewei
+ * @version: 
+ * @Date: 2021-08-21 22:37:11
+ * @LastEditors: xuewei
+ * @LastEditTime: 2021-08-24 21:19:06
+-->
+<!--  -->
+<template>
+  <div class="school">
+      <h2>学校名字: {{name}}</h2>
+      <h2>学校地址: {{address}}</h2>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name:'School',
+  data () {
+    return {
+        name:'blibli',
+        address:'无锡'
+    };
+  },
+}
+
+</script>
+
+<style scoped>
+  .school {
+    background-color: skyblue;
+    padding: 5px;
+    margin-top: 30px;
+  }
+</style>

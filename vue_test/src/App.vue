51043fee3712c5f04def9f706d7bdce414930101
xuewei111 <m13358109269_1@163.com>
2021-09-10 13:33:32 +0800

    20210910

--- vue_test/src/App.vue
+++ vue_test/src/App.vue
@@ -10,10 +10,15 @@
 <template>
   <div class="app">
     <h1>{{msg}}</h1>
+
     <!-- 通过父组件给子组件传递函数类型的props实现: 子给父传递数据-->
     <School :getSchoolName="getSchoolName"/>
-    <!-- 通过父组件给子组件绑定一个自定义事件实现:子给父传递数据-->
-    <Student @atguigu="getStudentName"/>
+
+    <!-- 通过父组件给子组件绑定一个自定义事件实现:子给父传递数据(第一种写法,使用@或v-on)-->
+    <Student @atguigu="getStudentName" @demo="m1"/>
+
+    <!-- 通过父组件给子组件绑定一个自定义事件实现:子给父传递数据(第二种写法,使用ref)-->
+    <!-- <Student ref="student"/> -->
   </div>
 </template>
 
@@ -35,7 +40,20 @@ export default {
     },
     getStudentName(name){
       console.log('App收到了学生名',name)
+    },
+    m1(){
+     console.log('demo事件被触发了！！！！') 
     }
+  },
+  mounted(){
+    // 等待3秒才去绑定atguigu
+    // setTimeout(()=>{
+    //   this.$refs.student.$on('atguigu',this.getStudentName)
+    // },3000)
+
+    // this.$refs.student.$on('atguigu',this.getStudentName)  // 绑定自定义事件
+    // this.$refs.student.$once('atguigu',this.getStudentName)  //绑定自定义事件(一次性)
+    
   }
 }
 
--- vue_test/src/components/Student.vue
+++ vue_test/src/components/Student.vue
@@ -12,6 +12,7 @@
       <h2>学生姓名: {{name}}</h2>
       <h2>学生性别: {{sex}}</h2>
       <button @click="sendStudentName">把学生名给App</button>
+      <button @click="unbind">解绑atguigu事件</button>
   </div>
 </template>
 
@@ -28,6 +29,11 @@ export default {
     sendStudentName(){
       // 触发Student组件实例身上的atguigu事件
       this.$emit('atguigu',this.name)
+      this.$emit('demo')
+    },
+    unbind(){
+      // this.$off('atguigu') //解绑一个自定义事件
+      this.$off(['atguigu','demo']) //解绑多个自定义事件
     }
   }
 }

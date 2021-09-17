66f0507f829dda8ca6d786ffd023fa2a8ce37871
xuewei111 <m13358109269_1@163.com>
2021-09-17 18:22:12 +0800

    2021091702

--- vue_test/src/components/Search.vue
+++ vue_test/src/components/Search.vue
@@ -1,5 +1,11 @@
 <template>
-  
+    <section class="jumbotron">
+        <h3>Search Github Users</h3>
+        <div>
+            <input type="text" placeholder="enter the name you search"/>&nbsp;
+            <button>Search</button>
+        </div>
+    </section>
 </template>
 
 <script>

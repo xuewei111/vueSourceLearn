62f8b29b94ab37e4fdd691f2ddc6133ac98b29e5
xuewei111 <m13358109269_1@163.com>
2021-09-23 14:27:20 +0800

    2021092301

---
+++ vue_test/src/components/Count.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+      <h1>当前求和为:{{sum}}</h1>
+      <select v-model="n">
+          <option :value="1">1</option>
+          <option :value="2">2</option>
+          <option :value="3">3</option>
+      </select>
+      <button @click="inCrement">+</button>
+      <button @click="deCrement">-</button>
+      <button @click="inCrementOdd">当前求和为奇数再加</button>
+      <button @click="inCrementWait">等一等再加</button>
+  </div>
+</template>
+
+<script>
+export default {
+    name:'Count',
+    data(){
+        return {
+            sum:0, // 当前的和
+            n:1 //用户选择的数字
+        }
+    },
+    methods:{
+        inCrement(){
+            this.sum += this.n
+        },
+        deCrement(){
+            this.sum -= this.n
+        },
+        inCrementOdd(){
+            if(this.sum % 2){
+                this.sum += this.n
+            }
+        },
+        inCrementWait(){
+            setTimeout(()=>{
+                this.sum += this.n
+            },500)
+        },
+    }
+}
+</script>
+
+<style>
+    button{
+        margin-left: 5px;
+    }
+</style>
